5de101de386ce751cdb2009264b15e5f3a8eb925
bcy <bcygsws@163.com>
2021-02-02 18:11:40 +0800

    keepalive的使用

--- src/components/keepalive/Coma.vue
+++ src/components/keepalive/Coma.vue
@@ -1,10 +1,3 @@
-<!--
- * @Description: 
- * @Author: bcygsws@163.com
- * @Date: 2021-02-02 18:08:07
- * @LastEditTime: 2021-02-02 18:09:30
- * @FilePath: \13-locallyBaoe:\Web-project\process22-vue-cliProject\02-code\vue-recall\src\components\keepalive\Coma.vue
--->
 <template>
   <div>
     <p>{{ msg }}</p>
