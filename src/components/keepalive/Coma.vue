accf1db83f011937d408ea9cf481149f249c91ac
bcy <bcygsws@163.com>
2021-02-02 19:26:09 +0800

    合并到master分支

--- src/components/keepalive/Coma.vue
+++ src/components/keepalive/Coma.vue
@@ -1,21 +1,6 @@
 <template>
   <div>
-<<<<<<< HEAD
-    <p>{{ msg }}</p>
-  </div>
-</template>
-<script>
-export default {
-  data() {
-    return {
-      msg: 'coma子组件'
-    };
-  }
-};
-</script>
-=======
     <h3>这是coma子组件</h3>
     <input type="text" name="coma">
   </div>
 </template>
->>>>>>> keepalive
