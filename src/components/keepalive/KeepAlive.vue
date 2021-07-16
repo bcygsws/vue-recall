bfac9de3874548e2fc59e21a85aac93eb9aa9d3f
BaoChengyi <bcygswspring168@gmail.com>
2021-07-17 02:21:18 +0800

    keep-alive内置组件的使用

--- src/components/keepalive/KeepAlive.vue
+++ src/components/keepalive/KeepAlive.vue
@@ -6,9 +6,13 @@
       <comb v-else></comb>
     </keep-alive>
     <input type="button" value="切换coma和comb组件的显示和隐藏，理解keep-alive的缓存机制" @click="switchCom" /> -->
-    <!-- max属性，缓存组件个数最多为10个；include属性设置后，仅coma 和comb的文本输入框中的内容，在动态组件切换可以保持，不被覆盖.comc中输入的内容会被覆盖-->
-    <keep-alive max="10" include="coma,comb">
-      <!-- <keep-alive max="10" include="['coma','comb']"> -->
+    <!-- max属性，缓存组件个数最多为10个；include属性设置后，仅coma 和comb的文本输入框中的内容，在动态组件切换可以保持，
+    不被覆盖.comc中输入的内容会被覆盖。但注意：如果使用正则表达式或者数组的方式，那么include属性要使用v-bind绑定，而使用使用
+    正常的,分割的方式，则无须动态绑定-->
+    <!-- <keep-alive max="10" include="coma,comb"> -->
+    <!-- <keep-alive max="10" :include="['coma','comb']"> -->
+    <!-- 或者使用下面的第三种方式： -->
+    <keep-alive max="10" :include="/coma|comb|comc/">
       <component :is="whichId"></component>
     </keep-alive>
     <input type="button" value="coma" @click="selected('a')" />
@@ -29,7 +33,7 @@ export default {
    * 2.keep-alive属性，有条件缓存的属性两个：exclude(不缓存哪些组件，优先级高于include)、include（缓存哪些组件）。
    * 属性值可以是字符串、数组和正则表达式，当属性值是数组或者正则表达式，需要使用v-bind动态绑定include或exclude,即
    * :include :exclude
-   * vue 2.5。0新增了一个max属性，用于限制最多可以缓存多少个组件实例
+   * vue 2.5.0新增了一个max属性，用于限制最多可以缓存多少个组件实例
    * 3.keep-alive的生命周期钩子activated、deactivated
    * 4.keep-alive中组件就不会执行beforeDestroy 和destroyed事件了，因为keep-alive会将组件缓存起来，而非销毁
    * Vue路由开启keep-alive时的注意点，https://www.jianshu.com/p/42429f4d8f9e
@@ -52,12 +56,12 @@ export default {
     console.log('mounted阶段了');
   },
   // keep-alive切换时，这两个生命周期钩子都会执行
-  // activated() {
-  //   console.log('activated钩子执行了');
-  // },
-  // deactivated() {
-  //   console.log('deactivated生命周期钩子也执行了');
-  // },
+  activated() {
+    console.log('activated钩子执行了');
+  },
+  deactivated() {
+    console.log('deactivated生命周期钩子也执行了');
+  },
   methods: {
     switchCom() {
       this.flag = !this.flag;
